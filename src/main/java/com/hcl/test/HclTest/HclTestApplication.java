1b5feae01c7eafc40ddc8bf40dce628561c03023
sopanbagalkar <sbagalkar@vignetcorp.com>
2020-11-01 16:35:35 +0530

    my first commit

---
+++ src/main/java/com/hcl/test/HclTest/HclTestApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.test.HclTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HclTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HclTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/test/HclTest/HclTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.test.HclTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HclTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
