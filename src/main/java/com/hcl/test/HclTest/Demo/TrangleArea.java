174ff5194a3e83c2c7e8b64f877466745fa43c02
sopanbagalkar <sbagalkar@vignetcorp.com>
2020-11-01 17:01:10 +0530

    Added Demo class

---
+++ src/main/java/com/hcl/test/HclTest/Demo/TrangleArea.java
@@ -0,0 +1,12 @@
+package com.hcl.test.HclTest.Demo;
+
+public class TrangleArea {
+    public static void main(String[] args) {
+        double base = 10;   //10 cm
+        double height = 12; //12 cm
+
+        //Area = (width*height)/2
+        double area = (base * height) / 2;
+        System.out.println("Area of Triangle is: " + area);
+    }
+}
